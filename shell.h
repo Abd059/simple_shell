658a32f08f9d09ac1b9f6f4944756d2bf823c2f0
Abd059 <kabdo7086@gmail.com>
2023-09-17 13:48:23 -0700

    main function

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+int _putchar(char c);
+int _csti(char *s); //converts a string into an integer
+void exit(char **ary); //exit the shell
+
+void emptyary(char **ary);
+
+
+#endif

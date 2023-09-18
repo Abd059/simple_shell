d001437bab9bf601ae282a4ad31f8095b9b3584b
Ahmedshaaban1114 <Ahmed@example.com>
2023-09-18 14:28:40 -0700

    ff

---
+++ checking.c
@@ -0,0 +1,98 @@
+#include "main.h"
+
+/**
+ * _checkpath - Check if @command_path is an absolute/relative path.
+ *
+ * @command_path: Path/command.
+ *
+ * Return: VALID_COMMAND for valid path, IS_REL_PATH for command name.
+*/
+int _checkpath(char *command_path)
+{
+	return (_checkfile(command_path));
+}
+
+/**
+ * _checkmode - Determine shell mode.
+ *
+ * Return: INTERACTIVE_MODE for interactive, NON_INTERACTIVE_MODE otherwise.
+*/
+int _checkmode(void)
+{
+	int mode;
+
+	mode = isatty(STDIN_FILENO);
+	if (mode == 1)
+	{
+		return (INTERACTIVE_MODE);
+	}
+	return (NON_INTERACTIVE_MODE);
+}
+
+/**
+ * _checkfile - Checks for the exisitence of the specified file at @path
+ *
+ * @path: The path to the file to be executed
+ *
+ * Return: VALID_COMMAND if @path is a valid file,
+ * NULL_FOUND if @path is NULL otherwise
+ * INVALID_COMMAND
+*/
+
+int _checkfile(char *path)
+{
+	if (path == NULL)
+	{
+		return (NULL_FOUND);
+	}
+	if (_isexec(path) == IS_EXEC)
+	{
+		if (access(path, F_OK) == VALID_COMMAND)
+		{
+			return (VALID_COMMAND);
+		}
+		return (INVALID_COMMAND);
+	}
+	if (_isrelpath(path) == IS_REL_PATH)
+	{
+		return (IS_REL_PATH);
+	}
+	if (access(path, F_OK == VALID_COMMAND)
+	{
+		return (VALID_COMMAND);
+	}
+	return (INVALID_COMMAND);
+}
+
+/**
+ * _checkenv - Check for the 'env' command and display env variables.
+ *
+ * @command: User's input command.
+ *
+ * Return: EXIT_SUCCESS for 'env', EXIT_FAILURE otherwise.
+*/
+int _checkenv(char *command)
+{
+	char env[] = "env";
+	int index = 0;
+
+	if (command == NULL)
+	{
+		return (EXIT_FAILURE);
+	}
+	while (env[index] != '\0')
+	{
+		if (command[index] != env[index])
+		{
+			return (EXIT_FAILURE);
+		}
+		index++;
+	}
+	if (command[index] != '\0')
+	{
+		return (EXIT_FAILURE);
+	}
+	_printenv();
+	signal_received = EXIT_SUCCESS;
+	return (EXIT_SUCCESS);
+}
---
+++ main.h
@@ -0,0 +1,61 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <signal.h>
+
+/**
+ * struct env_node - Node pointing to environment variable.
+ *
+ * @name: Variable's name.
+ * @value: Variable's value.
+ * @next: Next environment variable node.
+ *
+ * Description: Stores user's environment variables.
+*/
+typedef struct env_node
+{
+	char *name;
+	char *value;
+	struct env_node *next;
+} env_node_t;
+
+#define INTERACTIVE_MODE 1
+#define NON_INTERACTIVE_MODE 0
+#define NULL_FOUND -2
+#define INVALID_COMMAND 127
+#define VALID_COMMAND 0
+#define CTRL_C_SIGNAL_CODE 130
+#define IS_ABS_PATH 1
+#define IS_REL_PATH 2
+#define IS_EXEC 1
+#define IS_PATH 1
+#define IS_NOT_PATH 0
+#define ERROR_OCCURED -1
+#define SHLVL_NAME "SHLVL"
+#define SHLVL_VAL "1"
+#define EQUAL_STRING 0
+#define OVERWRITE 1
+#define IGNORE 0
+#define EXIT (_strcmp(commands[0], "exit") == EQUAL_STRING)
+
+char *_strcpy(char *dest, char *src);
+int _strlen(char *str);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+const char *_strstr(const char *haystack, const char *needle);
+int _checkenv(char *command);
+int _checkfile(char *path);
+int _checkmode(void);
+int _checkpath(char *command_path);
+int _prompt(char **buffer, size_t *bytes_read);
+
+#endif
+
